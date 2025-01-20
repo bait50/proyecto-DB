b21ddc31278ad4013ecdc46072ba0ac768877488
Dario Restrepo Landazury <drestrepo@unipacifico.edu.co>
2025-01-19 19:04:48 -0500

    Subiendo en NODE-MGDB

---
+++ NODE-MGDB-Proyect/config/dbPostgre.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+// Configura los parámetros de tu base de datos
+const conexP = new Pool({
+  user: 'postgres',          // Usuario de PostgreSQL
+  host: 'localhost',           // Dirección del servidor
+  database: 'yo soy producto',   // Nombre de la base de datos
+  password: 'postgresql',   // Contraseña del usuario
+  port: 5432                  // Puerto de conexión(por defecto es 5432)
+});
+
+conexP.connect((err, client, release) => {
+    if (err) {
+      return console.error('Error al conectar a la base de datos:', err.stack);
+    }
+    console.log('Conectado a la base de datos PostgreSQL');
+    release(); // Libera el cliente cuando no se usa
+  });
+  
+  module.exports = conexP;
